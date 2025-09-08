d42e16bce779d998a861b9c3d96b53f581b56ef2
GuillaumePOREZ72 <guillaume.porez@oclock.school>
2025-09-08 16:06:54 +0200

    feat: add initial project structure with TypeScript, React, and Tailwind CSS
    - Created main application files including components for Home, About, Navbar, and Title.
    - Added TypeScript configuration files for app and node.
    - Integrated Tailwind CSS and DaisyUI for styling.
    - Included SVG asset for TypeScript logo.
    - Set up Vite as the build tool with React plugin.

---
+++ src/components/About.tsx
@@ -0,0 +1,51 @@
+import Title from "./Title";
+import img from "../assets/portrait_officiel.png";
+import { LetterText, CalendarSync, Code } from "lucide-react";
+
+const aboutSections = [
+  {
+    id: 1,
+    title: "Développeur Frontend",
+    description:
+      "Je suis un développeur frontend spécialisé dans le développement d'applications web et mobiles.",
+    icon: <LetterText className="text-accent scale-150" />,
+  },
+  {
+    id: 2,
+    title: "Développeur Backend",
+    description:
+      "Je maîtrise les bases du développement backend pour créer des APIs robustes.",
+    icon: <CalendarSync className="text-accent scale-150" />,
+  },
+  {
+    id: 3,
+    title: "Développeur Fullstack",
+    description:
+      "J'ai une solide expérience en développement fullstack, ce qui me permet de concevoir des applications complètes.",
+    icon: <Code className="text-accent scale-150" />,
+  },
+];
+
+export default function About() {
+  return (
+    <div className="bg-base-300 p-10 mb-10 md:mb-32">
+      <Title title="À propos" />
+      <div className="md:h-screen flex justify-center items-center">
+        <div className="hidden md:block">
+          <img src={img} alt="" className="w-96 object-cover rounded-xl" />
+        </div>
+        <div className="md:ml-4 space-y-4">
+          {aboutSections.map((section) => (
+            <div key={section.id} className="flex flex-col md:flex-row items-center bg-base-100 p-5 rounded-xl md:w-96 shadow-xl">
+              <div className="mb-2 md:mb-1">{section.icon}</div>
+              <div className="md:ml-4 text-center md:text-left">
+                <h2 className="text-xl font-bold mb-1">{section.title}</h2>
+                <p className="text-sm">{section.description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,34 @@
+import { Container } from "lucide-react";
+
+export default function Navbar() {
+  return (
+    <div className="flex justify-center md:justify-between items-center p-4">
+      <a href="#" 
+      className="flex items-center font-bold text-3xl md:text-xl">
+        <Container className="mr-2" />
+        GP
+        <span className="text-accent">DEV</span>
+      </a>
+      <ul className="hidden md:flex space-x-4">
+        <li>
+          <a href="#" 
+          className="btn btn-sm btn-ghost">
+            Accueil
+          </a>
+          <a href="#" 
+          className="btn btn-sm btn-ghost">
+            À propos
+          </a>
+          <a href="#" 
+          className="btn btn-sm btn-ghost">
+            Mes expériences
+          </a>
+          <a href="#" 
+          className="btn btn-sm btn-ghost">
+            Mes projets
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
